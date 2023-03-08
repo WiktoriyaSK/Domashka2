512c2b9231bad2f562c2c44e1042f640587c591a
vika-sk <viktoriya-skryabina@mail.ru>
2023-03-08 21:51:46 +0300

    zadacha2

--- Program.cs
+++ Program.cs
@@ -1,25 +1,41 @@
-﻿// домашняя задача 1
-//Напишите программу, которая принимает на
-//вход трёхзначное число и на выход
-//показывает вторую цифру этого числа.
-//456 -> 5
-//782 -> 8
-//918 -> 1
+﻿
 
+//задача 2:нап прогр , кот выводит третью
+//цифру заданного числа или сообщает, что
+//третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
 int Prompt (string message)
 {
-    System.Console.Write(message);
+
+    Console.Write(message);
     string value = Console.ReadLine();
     int result = Convert.ToInt32(value);
     return result;
 }
-
-int number = Prompt("Введите трехзначное число > ");
-if (number < 100 || number >= 1000)
+int GetThirdRank(int number)
 {
-    Console.WriteLine("Вы ввели не трехзначное число, пожалуйста повторите ввод");
-    return;
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
 }
-Console.WriteLine($"Введенное число '{number} '");
-int secondRank = number / 10 % 10;
-Console.WriteLine($"Вторая цифра '{secondRank} '");
+
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+
+
+int number = Prompt("Введите число > ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRank(number));
+}
